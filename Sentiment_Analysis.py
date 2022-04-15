84a2e39b020fe00dce1b36ba72835dd2e0c7f7f3
Khaliq Ansari <khaliqansari771@gmail.com>
2022-04-16 02:45:36 +0530

    Completed code

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -3,8 +3,8 @@ import pickle
 import pandas as pd
 import webbrowser
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 import dash_bootstrap_components as dbc
 from matplotlib import pyplot as plt
 from dash.dependencies import Input, Output, State
